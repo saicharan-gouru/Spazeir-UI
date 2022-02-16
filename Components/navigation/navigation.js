7041688c924ea0609ca161f173d4ae6222d47a00
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-02-16 08:38:20 +0530

    paths fixation

--- Components/navigation/navigation.js
+++ Components/navigation/navigation.js
@@ -1,7 +1,7 @@
-const child = document.querySelectorAll(".child");
+const child = document.querySelectorAll(".nav-child");
 
 function Handler(eve) {
     for (let i = 0; i < child.length; i++)
-        child[i].className = child[i].className.replace(" active", "");
-    eve.currentTarget.className += " active";
+        child[i].className = child[i].className.replace(" nav-active", "");
+    eve.currentTarget.className += " nav-active";
 }
